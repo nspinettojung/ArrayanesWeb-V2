7bad73e3809c835ca78c7a8d1e811d0189535a93
Nico <nspinettojung@gmail.com>
2023-12-01 12:56:50 -0300

    navbar on scroll

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
   standalone: true,
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  imports:[CommonModule]
 })
 export class NavbarComponent {
 
+  @HostListener('window:scroll', ['$event'])
 
-  
+  onWindowScroll() {
+      let element = document.querySelector('.navbar') as HTMLElement;
+      if (window.scrollY > element.clientHeight) {
+        element.classList.add('solid-navbar');
+      } else {
+        element.classList.remove('solid-navbar');
+      }
+    }
 
 }
