421d2c0c13a6c65725ba0f05c66cad0ff3f9fbff
Nico <nspinettojung@gmail.com>
2023-12-02 20:14:13 -0300

    loadComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+        loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent)
     },
     {
         path: 'depto1',
-        loadComponent: () => import('./components/depto/depto1/depto1.component').then(m => m.Depto1Component)
+        loadComponent: () => import('./components/depto/depto1/depto1.component').then(c => c.Depto1Component)
     },
     {
         path: 'depto2',
-        loadComponent: () => import('./components/depto/depto2/depto2.component').then(m => m.Depto2Component)
+        loadComponent: () => import('./components/depto/depto2/depto2.component').then(c => c.Depto2Component)
     }
 ];
