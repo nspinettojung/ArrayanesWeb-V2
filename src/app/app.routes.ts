9d6d85494e5bda0c0b3313a0f007123d5cfb0eb9
Nico <nspinettojung@gmail.com>
2023-12-02 20:01:18 -0300

    borrando modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+        loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
     },
     {
         path: 'depto1',
-        loadChildren: () => import('./components/depto/depto1/depto1.module').then(m => m.Depto1Module)
+        loadComponent: () => import('./components/depto/depto1/depto1.component').then(m => m.Depto1Component)
     },
     {
         path: 'depto2',
-        loadChildren: () => import('./components/depto/depto2/depto2.module').then(m => m.Depto2Module)
+        loadComponent: () => import('./components/depto/depto2/depto2.component').then(m => m.Depto2Component)
     }
 ];
