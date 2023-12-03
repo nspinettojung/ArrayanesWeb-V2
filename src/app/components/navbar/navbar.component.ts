03b0a98859a46e232695ba26752cfaf9722c38ca
Nico <nspinettojung@gmail.com>
2023-12-03 09:58:37 -0300

    shadow-lg

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,21 +7,21 @@ import { Component, HostListener } from '@angular/core';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  imports:[CommonModule]
+  imports: [CommonModule]
 })
 export class NavbarComponent {
 
   @HostListener('window:scroll', ['$event'])
 
   onWindowScroll() {
-      let element = document.querySelector('.navbar') as HTMLElement;
-      if (window.scrollY > element.clientHeight) {
-        element.classList.add('solid-navbar', 'rounded-4', 'mx-3', 'shadow');
-        
-      } else {
-        element.classList.remove('solid-navbar', 'rounded-4', 'mx-3', 'shadow');
-        element.classList.add('sticky-top');
-      }
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.scrollY > element.clientHeight) {
+      element.classList.add('solid-navbar', 'rounded-4', 'mx-2', 'shadow-lg', 'sticky-top');
+
+    } else {
+      element.classList.remove('solid-navbar', 'rounded-4', 'mx-2', 'shadow-lg', 'sticky-top');
+
     }
+  }
 
 }
